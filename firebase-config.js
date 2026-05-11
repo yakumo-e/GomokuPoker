38cda9ab31a0fefa2717006edfbe6c00542a4959
yakumo-e <misuzu.mono@gmail.com>
2026-05-12 08:50:57 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 window.firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyByktW6Zx19v9XsvwuZybM95Nvb_ulP2-M",
+  authDomain: "gomokupoker.firebaseapp.com",
+  databaseURL: "https://gomokupoker-default-rtdb.firebaseio.com",
+  projectId: "gomokupoker",
+  storageBucket: "gomokupoker.firebasestorage.app",
+  messagingSenderId: "1095334995748",
+  appId: "1:1095334995748:web:f8d68584b8afe73d0c75b1",
 };
